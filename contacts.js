bbb5f623affbf046efc094391b3fa7ea1496860b
tania.voyat <tetiana.voyat@gmail.com>
2020-11-12 15:51:25 +0200

    adds changes to the hw 1

--- contacts.js
+++ contacts.js
@@ -3,49 +3,57 @@ const path = require("path");
 
 const contactsPath = path.resolve("db", "contacts.json");
 
-function listContacts() {
-  fs.readFile(contactsPath, "utf-8")
-    .then((data) => {
+async function listContacts() {
+  try {
+    await fs.readFile(contactsPath, "utf-8").then((data) => {
       const contacts = JSON.parse(data);
       contacts.map((contact) => console.table(contact));
-    })
-    .catch((err) => console.log(err.message));
+    });
+  } catch (err) {
+    console.log(err.message);
+  }
 }
 
-function getContactById(contactId) {
-  fs.readFile(contactsPath, "utf-8")
-    .then((data) => {
+async function getContactById(contactId) {
+  try {
+    await fs.readFile(contactsPath, "utf-8").then((data) => {
       const contacts = JSON.parse(data);
       contacts.map((contact) => {
         if (contactId === contact.id) {
           console.log(contact);
         }
       });
-    })
-    .catch((err) => console.log(err.message));
+    });
+  } catch (err) {
+    console.log(err.message);
+  }
 }
 
-function removeContact(contactId) {
-  fs.readFile(contactsPath, "utf-8")
-    .then((data) => {
+async function removeContact(contactId) {
+  try {
+    await fs.readFile(contactsPath, "utf-8").then((data) => {
       const contacts = JSON.parse(data);
       const filteredContacts = contacts.filter(
         (contact) => contact.id !== contactId
       );
       fs.writeFile(contactsPath, JSON.stringify(filteredContacts, null, 4));
-    })
-    .catch((err) => console.log(err.message));
+    });
+  } catch (err) {
+    console.log(err.message);
+  }
 }
 
-function addContact(name, email, phone) {
+async function addContact(name, email, phone) {
   const newContact = { Name: name, Email: email, Phone: phone };
-  fs.readFile(contactsPath, "utf-8")
-    .then((data) => {
+  try {
+    await fs.readFile(contactsPath, "utf-8").then((data) => {
       const contacts = JSON.parse(data);
       contacts.push(newContact);
       fs.writeFile(contactsPath, JSON.stringify(contacts, null, 4));
-    })
-    .catch((err) => console.log(err.message));
+    });
+  } catch (err) {
+    console.log(err.message);
+  }
 }
 
 module.exports = {
