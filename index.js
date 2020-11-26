6e5a4e626495f72b4e4294e3558c93c1070b81db
tania.voyat <tetiana.voyat@gmail.com>
2020-11-26 15:19:25 +0200

    adds error resolve

--- index.js
+++ index.js
@@ -8,6 +8,15 @@ app.use(cors());
 app.use(morgan("combined"));
 app.use(express.json());
 app.use("/api/contacts", router);
+app.use((err, req, res, next) => {
+  err.status = err.status ? err.status : HttpCode.INTERNAL_SERVER_ERROR;
+  res.status(err.status).json({
+    status: err.status === 500 ? "fail" : "error",
+    code: err.status,
+    message: err.message,
+    data: err.status === 500 ? "Internal Server Error" : err.data,
+  });
+});
 
 const PORT = 3000;
 app.listen(PORT, () => {
