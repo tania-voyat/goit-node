6a9bf9f524b76cdc37ec54fe344f544f143bc785
tania.voyat <tetiana.voyat@gmail.com>
2020-11-28 16:25:56 +0200

    adds changes to hw3

--- index.js
+++ index.js
@@ -6,28 +6,30 @@ const app = express();
 const router = require("./api/routers/contactsRouters");
 const mongoose = require("mongoose");
 require("dotenv").config();
-const PORT = 3000;
 
 async function main() {
-  app.use(cors());
-  app.use(morgan("combined"));
-  app.use(express.json());
-  app.use("/api/contacts", router);
-  app.use((err, req, res, next) => {
-    err.status = err.status ? err.status : HttpCode.INTERNAL_SERVER_ERROR;
-    res.status(err.status).json({
-      status: err.status === 500 ? "fail" : "error",
-      code: err.status,
-      message: err.message,
-      data: err.status === 500 ? "Internal Server Error" : err.data,
+  try {
+    app.use(cors());
+    app.use(morgan("combined"));
+    app.use(express.json());
+    app.use("/api/contacts", router);
+    app.use((err, req, res, next) => {
+      err.status = err.status ? err.status : HttpCode.INTERNAL_SERVER_ERROR;
+      res.status(err.status).json({
+        status: err.status === 500 ? "fail" : "error",
+        code: err.status,
+        message: err.message,
+        data: err.status === 500 ? "Internal Server Error" : err.data,
+      });
     });
-  });
-  await mongoose.connect(
-    "mongodb+srv://Admin:Iwoastfwya123@cluster0.9uvyy.mongodb.net/db-contacts?retryWrites=true&w=majority"
-  );
-  app.listen(PORT, () => {
-    console.log("Database connection successful");
-  });
+    await mongoose.connect(process.env.MONGO_DB_URL);
+    app.listen(process.env.PORT, () => {
+      console.log("Database connection successful");
+    });
+  } catch (err) {
+    console.log(err);
+    process.exit(1);
+  }
 }
 
 main();
